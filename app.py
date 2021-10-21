ebc2476aa413c5bd24159e43b2a28157f8bb2464
lillianlakes <lillian.lakes@gmail.com>
2021-10-21 09:02:56 -0700

    adds comment about secret key

--- app.py
+++ app.py
@@ -24,9 +24,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+## for heroku:
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
 # added line below
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 toolbar = DebugToolbarExtension(app)
+
 
 connect_db(app)
 
